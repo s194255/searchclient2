1412afab71e21144b6eed229e5ce5b91506f53a7
Christian <s194255@student.dtu.dk>
2022-04-02 10:54:58 +0200

    Update helper_improved.py

--- searchclient/agent_types/helper_improved.py
+++ searchclient/agent_types/helper_improved.py
@@ -14,47 +14,54 @@ import sys
 
 from domains.hospital import HospitalGoalDescription
 from search_algorithms.graph_search import graph_search
+from domains.hospital.actions import NoOpAction
 from utils import *
 import time
 import copy
 
+def get_actor_path(plan, level, actor_index, current_state, time_step):
+    actor_pos, actor_char = current_state.agent_positions[actor_index]
+    actor_color = level.colors[actor_char]
 
-def helper(plan, level, actor_index, current_state, actor_goal_description):
-    pos_actor, actor_char = current_state.agent_positions[actor_index]
+    path = [actor_pos]
+    for i in range(time_step, len(plan)):
+        action = plan[i][actor_index]
+        actor_pos = action.calculate_agent_positions(actor_pos)
+        path.append(actor_pos)
+    return path
 
-    # get future positions according to the plan
-    future_pos = []
-    for i in range(len(plan)):
-        action = plan[i][0]
-        pos_actor = action.calculate_agent_positions(pos_actor)
-        future_pos.append(pos_actor)
 
-    # Get color of the actor
-    actor_color = level.colors["0"]
 
-    new_goals = []
+def get_helper_goals(path, level, current_state, agent_goals, helper_char):
+    # Get color of the helper
+    helper_color = level.colors[helper_char]
 
-    char = current_state.object_at(future_pos[0])
-    if char == '':
-        pass
-    else:
-        if level.colors[char] == actor_color:
-            char = current_state.object_at(future_pos[1])
+    helper_goals = set([])
 
-        for i in range(len(future_pos)):
-            pos = future_pos[i]
-            # make path spaces negative goals for specific helper
-            new_goal = (pos, char, False)
-            # goal_description.goals.append(new_goal)
-            new_goals.append(new_goal)
+    box_chars = get_boxes_with_color(current_state, level, helper_color)
 
-            #make goal spaces negative goals for specific helper
-            for (actor_goal_pos, _, _) in actor_goal_description.goals:
-                new_goals.append((actor_goal_pos, char, False))
+    for pos in path:
+        helper_goals.add( (pos, helper_char, False))
+        for box_char in box_chars:
+            helper_goals.add((pos, box_char, False))
 
-            # print(new_goals, file=sys.stderr)
+    for (pos, char, isPositive) in agent_goals["0"]:
+        helper_goals.add( (pos, helper_char, False))
 
-    return HospitalGoalDescription(level, new_goals)
+        for box_char in box_chars:
+            helper_goals.add((pos, box_char, False))
+
+    return helper_goals
+
+    #apply the negative goals to the agent herself
+
+def get_boxes_with_color(current_state, level, color):
+    box_chars = []
+    for (box_pos, box_char) in current_state.box_positions:
+        box_color = level.colors[box_char]
+        if box_color == color:
+            box_chars.append(box_char)
+    return box_chars
 
 
 def get_conflicting_helper(current_state, plan, agent_idx, level, time_step):
@@ -63,44 +70,40 @@ def get_conflicting_helper(current_state, plan, agent_idx, level, time_step):
     # Get color of the actor
     agent_color = level.colors[agent_char]
 
-    future_pos = []
-    for i in range(time_step, time_step+2):
-        action = plan[i][agent_idx]
-        print("action", action, file=sys.stderr)
-        agent_pos = action.calculate_agent_positions(agent_pos)
-        future_pos.append(agent_pos)
+    action = plan[time_step][agent_idx]
+    next_pos = action.calculate_agent_positions(agent_pos)
+    temp_char = current_state.object_at(next_pos)
 
 
-    #quick check to see if we are bumping into our own box
-    temp_char = current_state.object_at(future_pos[0])
-    temp_color = level.colors[temp_char]
-
-    if temp_color == agent_color:
-        box_idx, box_char = current_state.box_at(future_pos[1])
-        helper_idx, helper_char = current_state.agent_at(future_pos[1])
-
-    else:
-        box_idx, box_char = current_state.box_at(future_pos[0])
-        helper_idx, helper_char = current_state.agent_at(future_pos[0])
-
-
-
-    if box_char == '':
-        #we know it is a helper
-
+    if '0' <= temp_char <= '9':
+        helper_idx, helper_char = current_state.agent_at(next_pos)
         helper_color = level.colors[helper_char]
         return helper_color, helper_char, helper_idx
 
-
-    elif helper_char == '':
-        # we now it is a box
+    elif 'A' <= temp_char <= 'Z':
+        box_idx, box_char = current_state.box_at(next_pos)
         box_color = level.colors[box_char]
-        # print("kassen har den her farve", box_color, file=sys.stderr)
+
+        if box_color == agent_color:
+            next_box_pos = action.calculate_box_positions(next_pos)
+
+
+
+            temp_char = current_state.object_at(next_box_pos)
+            if '0' <= temp_char <= '9':
+                helper_idx, helper_char = current_state.agent_at(next_box_pos)
+                helper_color = level.colors[helper_char]
+                return helper_color, helper_char, helper_idx
+
+            elif 'A' <= temp_char <= 'Z':
+                box_idx, box_char = current_state.box_at(next_box_pos)
+                box_color = level.colors[box_char]
+
+
         for i in range(level.num_agents):
             _, helper_char = current_state.agent_positions[i]
             helper_color = level.colors[helper_char]
             if helper_color == box_color:
-                print("kalder på agent med farve {}".format(helper_color), file=sys.stderr)
                 helper_idx = i
                 break
         return helper_color, helper_char, helper_idx
@@ -109,41 +112,13 @@ def get_conflicting_helper(current_state, plan, agent_idx, level, time_step):
         return False, False, False
 
 
-    # helper_char = current_state.object_at(future_pos[0])
-    #
-    #
-    # if helper_char == '':
-    #     return False, False, False
-    # else:
-    #
-    #     if level.colors[helper_char] == agent_color:
-    #         helper_char = current_state.object_at(future_pos[1])
-    #
-    #
-    #
-    #     helper_color = level.colors[helper_char]
-    #     for i in range(level.num_agents):
-    #         _, temp_char = current_state.agent_positions[i]
-    #         if temp_char == helper_char:
-    #             helper_idx = i
-    #
 
-    #
-    #     return helper_color, helper_char, helper_idx
-
-def get_joint_action(plan, time_step, indices, level, current_state):
-    # print("-------------", file=sys.stderr)
-    # print(plan, file=sys.stderr)
-    # print(time_step, file=sys.stderr)
-    # print(indices, file=sys.stderr)
-    # print("-------------", file=sys.stderr)
-
-
+def get_joint_action(plan, time_step, controllable_agents, level, current_state):
 
     joint_action = []
     for agent_idx in range(level.num_agents):
         agent_pos, agent_char = current_state.agent_positions[agent_idx]
-        if agent_idx in indices:
+        if agent_char in controllable_agents:
             joint_action.append(plan[time_step][agent_idx])
         else:
             joint_action.append(GenericNoOp())
@@ -182,6 +157,70 @@ def convert_plan(plan, current_state, monochrome_state):
     #     for j in range(monochrome_state.agent_positions):
     #         pos_temp, char_temp = current_state.agent_positions[i]
 
+def make_naive_plan(current_state, agent_goals, controllable_agents, level, frontier, action_library):
+    agent_plans = []
+    planning_successes = []
+    longest_plan = 0
+    for i in range(level.num_agents):
+        agent_pos, agent_char = current_state.agent_positions[i]
+        actor_color = level.colors[agent_char]
+        mono_state = current_state.color_filter(actor_color)
+        mono_goaldes = HospitalGoalDescription(level, agent_goals[agent_char])
+        action_set = [[GenericNoOp()]] * level.num_agents
+        action_set[i] = action_library
+
+
+        planning_success, agent_plan = graph_search(mono_state, action_set, mono_goaldes, frontier)
+        agent_plans.append(agent_plan)
+        planning_successes.append(planning_success)
+        if len(agent_plan) > longest_plan:
+            longest_plan = len(agent_plan)
+
+    plan = []
+    for time_step in range(longest_plan):
+        joint_action = []
+        for i in range(level.num_agents):
+            agent_plan = agent_plans[i]
+            if time_step < len(agent_plan):
+                joint_action.append( agent_plan[time_step][i])
+            else:
+                joint_action.append(GenericNoOp())
+        plan.append(joint_action)
+    return planning_successes[0], plan
+
+
+
+
+
+def make_plan(current_state, agent_goals, controllable_agents, level, frontier, action_library):
+    colors = []
+    ggoals = []
+    for agent_char in controllable_agents:
+        colors.append(level.colors[agent_char])
+        if agent_char != "0":
+            ggoals.extend(agent_goals[agent_char])
+
+    mono_state = current_state.color_filters(colors)
+    mono_goaldes = HospitalGoalDescription(level, ggoals)
+
+
+    action_set = [action_library] * len(mono_state.agent_positions)
+
+
+    planning_success, plan = graph_search(mono_state, action_set, mono_goaldes, frontier)
+    if planning_success == False:
+        pass
+    plan = convert_plan(plan, current_state, mono_state)
+
+    return planning_success, plan
+
+def prune_controllable_agents(controllable_agents, actor_path, current_state):
+
+    for (agent_pos, agent_char) in current_state.agent_positions:
+        if agent_char in controllable_agents:
+            if agent_pos not in actor_path:
+                controllable_agents.remove(agent_char)
+            #todo vi mangler at holde øje med kasserne
 
 
 def helper_improved_agent_type(level, initial_state, action_library, actor_goal_description, frontier):
@@ -204,90 +243,95 @@ def helper_improved_agent_type(level, initial_state, action_library, actor_goal_
     action_set = [[GenericNoOp()]] * level.num_agents
     action_set[actor_index] = action_library
 
-    #Get color of the actor
-    actor_color = level.colors["0"]
+
     current_state = initial_state
 
-    goals = []
+    actor_goals = set([])
 
 
     for sub_goal in actor_goal_description.goals:
-        colors = set([actor_color])
-        indices = [actor_index]
-        # action_set = [[GenericNoOp()]] * level.num_agents
-        # action_set[actor_index] = action_library
 
-        goals.append(sub_goal)
-        goal_description = HospitalGoalDescription(level, goals)
 
-        #while sub_goal is not solved
-        while True:
+        actor_goals.add(sub_goal)
+        goal_des = HospitalGoalDescription(level, actor_goals)
 
-            #Get the monochrome problem
-            monochrome_problem = current_state.color_filters(colors)
-            monochrome_goal_description = goal_description.color_filters(colors)
+        controllable_agents = set(["0"])
 
-            action_set = [action_library] * len(monochrome_problem.agent_positions)
+        agent_goals = {}
+        for agent_pos, agent_char in copy.deepcopy(current_state).agent_positions:
+            agent_goals[agent_char] = []
+        agent_goals["0"] = actor_goals
 
-            print("-------------",file=sys.stderr)
-            print("mono state", monochrome_problem, file=sys.stderr)
-            print("colors", colors, file=sys.stderr)
-            print("action set", action_set, file=sys.stderr)
-            print("-------------", file=sys.stderr)
+        hårdknudefaktor = 0
 
-            planning_success, plan = graph_search(monochrome_problem, action_set, monochrome_goal_description, frontier)
-            plan = convert_plan(plan, current_state, monochrome_problem)
-
-            if planning_success == False:
-                print("execution faulted", file=sys.stderr)
-
-            if (planning_success == True) and (len(plan) == 0):
-                break
-
+        while goal_des.is_goal(copy.deepcopy(current_state)) == False:
+            if hårdknudefaktor < 1:
+                print("-------------LIGE NAIV PLAN-------------", file=sys.stderr)
+                planning_success, plan = make_naive_plan(copy.deepcopy(current_state), agent_goals, controllable_agents,
+                                                   level, frontier, action_library)
+            else:
+                print("-------------LIGE HÅRDKNUDEPLAN-------------", file=sys.stderr)
+                planning_success, plan = make_plan(copy.deepcopy(current_state), agent_goals, controllable_agents,
+                                                   level, frontier, action_library)
+            print("-------------LIGE LAVET NY PLAN-------------", file=sys.stderr)
+            print("current_state", current_state, file=sys.stderr)
+            print("agent goals", agent_goals, file=sys.stderr)
+            print("controllable_agents", controllable_agents, file=sys.stderr)
+            print("plaaaan", plan, file=sys.stderr)
 
 
             # while actor succeeds in actions
             time_step = 0
             while time_step < len(plan):
-                # print(plan, file=sys.stderr)
-                joint_action = get_joint_action(plan, time_step, indices, level, current_state)
-                # print("---------", file=sys.stderr)
-                # print("joint action", joint_action, file=sys.stderr)
-                # print("---------", file=sys.stderr)
-                # pass the joint_action to the server
+                joint_action = get_joint_action(plan, time_step,
+                                                controllable_agents, level, copy.deepcopy(current_state))
+                print("-------------NYT TIDSSKRIDT-------------", file=sys.stderr)
+                print("current_state", current_state, file=sys.stderr)
+
+
                 print(joint_action_to_string(joint_action), flush=True)
                 execution_successes = parse_response(read_line())
+
 
                 # applicable actions will be used to update current_state
                 applicable_actions = []
                 for agent_idx in range(level.num_agents):
+                    agent_pos, agent_char = copy.deepcopy(current_state).agent_positions[agent_idx]
+
                     if execution_successes[agent_idx] == False:
-                        # if action is illegal, just do GenericNoOp()
                         applicable_actions.append(GenericNoOp())
 
-                        if agent_idx in indices:
-                            print("agent {} har et problem".format(agent_idx), file=sys.stderr)
-                            color_conflict, char_conflict, idx_conflict = get_conflicting_helper(current_state, plan,
-                                                                                                 agent_idx, level, time_step)
-                            print("color conflict {}".format(color_conflict), file=sys.stderr)
+                        if agent_char in controllable_agents:
+                            color_conflict, char_conflict, idx_conflict = get_conflicting_helper(copy.deepcopy(current_state), plan,
+                                                                                                 agent_idx, level,
+                                                                                                 time_step)
                             if color_conflict != False:
-                                colors.add(color_conflict)
-                                indices.append(idx_conflict)
-                                # action_set[idx_conflict] = action_library
-
+                                controllable_agents.add(char_conflict)
+                                actor_path = get_actor_path(plan, level, actor_index,
+                                                            copy.deepcopy(current_state), time_step)
+                                negative_goals = get_helper_goals(actor_path, level,
+                                                                  copy.deepcopy(current_state),
+                                                                  agent_goals, char_conflict)
+                                agent_goals[char_conflict] = negative_goals
                     else:
                         applicable_actions.append(joint_action[agent_idx])
 
                 # current_state is updated based on legal moves only
+                print("current_state", current_state, file=sys.stderr)
                 current_state = current_state.result(applicable_actions)
-                # print(current_state, file=sys.stderr)
+                # time.sleep(1)
+
 
                 time_step += 1
 
                 # If actor failed action, then we have to replan
                 if execution_successes[actor_index] == False:
-                    print("jeg fejlede en handling", file=sys.stderr)
+                    hårdknudefaktor += 1
+                    prune_controllable_agents(controllable_agents, actor_path, copy.deepcopy(current_state))
                     break
+                else:
+                    # todo hvad hvis helperen er den sidste der bevæger sig?
+                    hårdknudefaktor = 0
 
 
 
@@ -307,38 +351,22 @@ def helper_improved_agent_type(level, initial_state, action_library, actor_goal_
 
 
 
-            #
-            #
-            #
-            # #loop until actor is allowed perform an action
-            # while True:
-            #     #loop through all agents. Get joint_action (some of those actions might be illegal)
-            #     joint_action = []
-            #     for i in range(level.num_agents):
-            #         agent_pos, agent_char = current_state.agent_positions[i]
-            #
-            #         #If agent is a helper
-            #         if agent_char != "0":
-            #             helper_color = level.colors[agent_char]
-            #
-            #             #only the helper is allowed to move
-            #             action_set_helper = [[GenericNoOp()]] * level.num_agents
-            #             action_set_helper[i] = action_library
-            #
-            #             #The helper only has to deal with the goals relevant for her
-            #             monochrome_goal_description_helper = helper_goal_description.color_filter(helper_color)
-            #             planning_success, plan = graph_search(current_state, action_set_helper,
-            #                                                   monochrome_goal_description_helper, frontier)
-            #
-            #             #If plan is empty, just do GenericNoOp()
-            #             if len(plan) == 0:
-            #                 joint_action.append(GenericNoOp())
-            #             else:
-            #                 #get action in plan corresponding to that helper. All the other agents are doing GenericNoOp()
-            #                 helper_action = plan[0][i]
-            #                 joint_action.append(helper_action)
-            #         elif agent_char == "0":
-            #             joint_action.append(actor_action[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
